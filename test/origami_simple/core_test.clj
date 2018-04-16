5c4bff5b073411fadfbd6155b89368e6ae4c36ca
Nicolas Modrzyk <nico@karabiner-software.com>
2018-04-17 01:30:28 +0900

    with cat

--- test/origami_simple/core_test.clj
+++ test/origami_simple/core_test.clj
@@ -9,3 +9,13 @@
 (deftest a-test
   (testing "simple opencv test."
    (let [mat (new-mat 3 3 CV_8UC1 (new-scalar 150 150 150))]) ))
+
+(deftest b-test
+   (testing "jpg, png and canny test."
+	(->
+	 (imread "cat_in_bowl.jpeg")
+	 (cvt-color! COLOR_RGB2GRAY)
+	 (canny! 300.0 100.0 3 true)
+	 (bitwise-not!)
+	 (u/resize-by 0.5)
+	 (imwrite "canny-cat.jpg"))))
