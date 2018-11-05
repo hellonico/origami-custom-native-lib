875e61a03030d630cbeb4d4611e06021d31a53f6
vagrant <vagrant@debian>
2018-11-06 08:37:54 +0900

    where did the code go ?

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :url "http://example.com/FIXME"
+  :main simple.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [   
---
+++ src/simple/core.clj
@@ -0,0 +1,6 @@
+
+(ns simple.core
+ (:require [opencv4.core :refer :all]))
+
+(defn -main [& args]
+  (println "Using OpenCV Version: " VERSION ".."))
