c0dc008faa7019491b197c1efdc21e0a1c5cc3ca
Nicolas Modrzyk <nico@karabiner-software.com>
2018-11-06 12:15:02 +0900

    bw cat

--- src/simple/core.clj
+++ src/simple/core.clj
@@ -1,6 +1,11 @@
 
 (ns simple.core
- (:require [opencv4.core :refer :all]))
+ (:require 
+ 	[opencv4.utils :as u] 
+ 	[opencv4.core :refer :all]))
 
 (defn -main [& args]
-  (println "Using OpenCV Version: " VERSION ".."))
+  (println "Using OpenCV Version: " VERSION "..")
+  (-> "cat_in_bowl.jpeg"
+    (imread IMREAD_REDUCED_GRAYSCALE_4)
+    (imwrite "cat_bw.jpeg")))
